a49fd69e5fbb813fed2653704404343ea89ac941
github username <github email address>
2020-04-14 14:21:18 -0500

    Tuesday

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -3,7 +3,7 @@ class DescriptionsController < ApplicationController
     def create 
         my_location = Geocoder.search(params['location'])
         array = my_location.first.coordinates
-        breweries = Brewery.near(params['location'])
+        breweries = Brewery.near(params['location'], 50)
         render json: {breweries: breweries, location: array}
     end 
 end
