05c800b9efc76f00e78730f535aa1522ceddc247
daveholmes88 <github email address>
2020-06-10 20:36:26 -0500

    admin fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_210831) do
+ActiveRecord::Schema.define(version: 2020_06_11_004239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_210831) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "email"
+    t.boolean "admin", default: false
   end
 
 end
