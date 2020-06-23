2df8a77796f26e5a1643a8ad626033a223e46d6a
daveholmes88 <github email address>
2020-06-23 13:26:49 -0500

    update fix

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -39,7 +39,7 @@ class BreweriesController < ApplicationController
             phone: params['brewery']['phone'], 
             website: params['brewery']['website'], 
             latitude: params['brewery']['latitude'],
-            longitude: params['brewery']['longitude']),
+            longitude: params['brewery']['longitude'])
         render json: brewery
     end 
 
