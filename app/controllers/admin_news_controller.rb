1d8b75e4658885ad129715475184020e51ab13fe
daveholmes88 <github email address>
2020-08-16 13:50:18 -0500

    start admin

---
+++ app/controllers/admin_news_controller.rb
@@ -0,0 +1,36 @@
+class AdminNewsController < ActionController::API
+
+    def index
+        admin_news = AdminNew.all
+        users = User.all
+        render json: { breweries: admin_news, users: users }
+    end 
+
+    def create
+        @admin_new = {
+            name: params['name'],
+            brewery_type: params['kind'],                
+            address: params['address'], 
+            city: params['city'],
+            state: params['state'],
+            zip: params['zip_code'],
+            country: params['country'],
+            phone: params['phone_number'],
+            website: params['website']
+        }
+        admin_new = AdminNew.create(@admin_new)
+        if admin_new.valid? 
+            render json: admin_new
+        else 
+            render json: { error: 'failed to create brewery' }, status: :not_acceptable
+        end 
+    end 
+
+    def destroy
+        brewery = AdminNew.find(params['id'])
+        brewery.destroy
+        breweries = AdminNew.all
+        render json: breweries
+    end 
+
+end
---
+++ db/migrate/20200814174205_create_admin_news.rb
@@ -0,0 +1,19 @@
+class CreateAdminNews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :admin_news do |t|
+      t.string :name
+      t.string :brewery_type
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.string :country
+      t.float :longitude
+      t.float :latitude
+      t.string :phone
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
